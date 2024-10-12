f6a1e0d4a7f0083bf19fe5e22f2df77df145b3a0
adedayo-888 <dayoadeniji69@yahoo.com>
2024-10-12 21:18:33 +0100

    foundation commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,18 @@
+
+
+const Home =()=>{
+    return(
+        <div
+        className='
+        bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        overflow-hidden
+        overflow-y-auto
+      '
+    >
+       <h1 className={'text-3xl text-green-400'}> music-screen</h1>
+    </div>)
+}
+export default Home
