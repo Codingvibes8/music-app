7a10c6835e0d161f39207e8b4bf84ea42cc93ad4
adedayo-888 <dayoadeniji69@yahoo.com>
2024-10-13 03:56:40 +0100

    Header and  Home Page added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,9 +8,9 @@ import {usePathname} from 'next/navigation';
 // import { Song } from '@/types';
 // import usePlayer from '@/hooks/usePlayer';
 
-// import SidebarItem from './SidebarItem';
-// import Box from './Box';
-// import Library from './Library';
+
+
+import Library from './Library';
 import React, {useMemo} from 'react';
 import SidebarItem from "@/components/SidebarItem";
 import Box from "@/components/Box";
@@ -46,6 +46,7 @@ const Sidebar = ({children}: SidebarProps) => {
             className={twMerge(`flex h-full`,)}>
             <div className='md:flex hidden flex-col gap-y-2 bg-black  h-full
                 w-[300px] p-2'>
+                <div className={'flex items-center justify-center w-full gap-x-2 text-3xl text-yellow-300 font-bold'}>MusicShark</div>
                 <Box className='flex flex-col gap-y-4 px-5 py-4'>
                     {routes.map((item) => (
                         <SidebarItem key={item.label} {...item} />
@@ -53,7 +54,7 @@ const Sidebar = ({children}: SidebarProps) => {
                 </Box>
 
                 <Box className='overflow-y-auto h-full'>
-                    Library
+                    <Library/>
                 </Box>
             </div>
             <main className='h-full flex-1 overflow-y-auto py-2'>{children}</main>
